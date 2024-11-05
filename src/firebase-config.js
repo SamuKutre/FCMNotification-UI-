95d9c9c17da16ce3895315746ffa38740471543d
Samruddhi S K <samruddhisk@integramicro.co.in>
2024-11-05 12:27:00 +0530

    updated

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -23,24 +23,24 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, isSupported } from "firebase/messaging";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCeIY5HBbPF4aKCLeTBzJWBC7pQuTNFqm8",
-//   authDomain: "xenon-timer-393911.firebaseapp.com",
-//   projectId: "xenon-timer-393911",
-//   storageBucket: "xenon-timer-393911.appspot.com",
-//   messagingSenderId: "164993453767",
-//   appId: "1:164993453767:web:574f2a5d1ca8b76eb378e2",
-//   measurementId: "G-3J9M7KSE8J"
-// };
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  };
+  apiKey: "AIzaSyCeIY5HBbPF4aKCLeTBzJWBC7pQuTNFqm8",
+  authDomain: "xenon-timer-393911.firebaseapp.com",
+  projectId: "xenon-timer-393911",
+  storageBucket: "xenon-timer-393911.appspot.com",
+  messagingSenderId: "164993453767",
+  appId: "1:164993453767:web:574f2a5d1ca8b76eb378e2",
+  measurementId: "G-3J9M7KSE8J"
+};
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+//   };
   
 
 const app = initializeApp(firebaseConfig);
